fe7dd22b5df170f0267b3a4610deb039e39cbae4
daahuang <andrew800619@gmail.com>
2015-09-29 12:51:36 -0700

    update matlab_func_batch

--- matlab_func_batch.sh
+++ matlab_func_batch.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
+#PBS -l nodes=1:ppn=2
+#PBS -l walltime=24:00:00
+#PBS -l mem=16gb
+#PBS -V
+
 
 # Change to the submission directory
-cd $PBS_O_WORKDIR
+if [ "$PBS_O_WORKDIR" != "" ];then
+  cd $PBS_O_WORKDIR
+fi
 
 
 # Run the m-file
-#~/matlab -nojvm -nodisplay -r "${1}(${2}); exit;" > matlab_${2}.out
+#~/matlab -nojvm -nodisplay -r "${func}(${arg}); exit;" > matlab_${2}.out
 
 # Start MATLAB
-~/matlab -nojvm -nodisplay > matlab_${2}.out << EOF
+~/matlab -nojvm -nodisplay > matlab_${arg}.out << EOF
 
 % Run the function
-${1}(${2});
+${func}(${arg});
 
 % Exit MATLAB
 exit
 EOF
 
 # Display the output
-cat matlab_${2}.out
+cat matlab_${arg}.out
